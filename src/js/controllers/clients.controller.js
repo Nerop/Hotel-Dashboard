67197b1e3107f330a96f4ae2a59ca1e4407e0db0
Vladyslav Sazonov <nerop30@gmail.com>
2016-11-11 13:50:53 +0200

    data appends to table

--- src/js/controllers/clients.controller.js
+++ src/js/controllers/clients.controller.js
@@ -1,4 +1,13 @@
 scotchApp.controller('clientsController', function ($scope) {
     $scope.message = 'Клиенты';
+    $.getJSON("../2m/src/js/server/table.json", function(data){
+        for (let i = 0; i < data.length; i++){
+            console.log(data[i]);
+            $('.clients-table').append('<tr>' + '<td>' + data[i].id + '</td>' +'<td>' + data[i].surname + '</td>' +'<td>' + data[i].name + '</td>' +'<td>' + data[i].patron + '</td>' +'<td>' + data[i].phone_number + '</td>' + '</tr>');
+        }
+        // console.log(data.length);
 
+
+
+    }); // <=== was missing
 });
