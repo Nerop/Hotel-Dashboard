91337943fa6b3ac5683d81081df8d2a4bb846956
Vladyslav Sazonov <nerop30@gmail.com>
2016-11-11 12:04:08 +0200

    added simple server

---
+++ src/js/server/server.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'hotel'
+});
+
+function getSQL(callback) {
+    connection.connect();
+    let json = '';
+    let query = 'SELECT * FROM clients';
+    connection.query(query, function(err, results, fields) {
+        if (err)
+            return callback(err, null);
+
+        console.log('The query-result is: ', results[0]);
+
+        json = JSON.stringify(results);
+
+        connection.end();
+        console.log('JSON-result:', json);
+        callback(null, json);
+    });
+}
+
+http.createServer(function(req, res) {
+  getSQL(function(err, result) {
+    res.writeHead(200, {
+      'Content-Type' : 'x-application/json'
+    });
+    res.end(result);
+  });
+}).listen(3000);
