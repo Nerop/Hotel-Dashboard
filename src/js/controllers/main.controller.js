31ebcbfb4565f696924672fa17fd22acf916f834
Vladyslav Sazonov <nerop30@gmail.com>
2016-11-11 12:15:06 +0200

    json response

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -1,4 +1,6 @@
 scotchApp.controller('mainController', function ($scope) {
     $scope.message = 'Добро пожаловать.';
-
+    $.getJSON("../2m/src/js/server/table.json", function(data){
+        console.log(data);
+    }); // <=== was missing
 });
--- src/js/server/server.js
+++ src/js/server/server.js
@@ -1,23 +1,27 @@
 const http = require("http");
-const mysql      = require('mysql');
+const mysql = require('mysql');
+const fs = require('fs');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'hotel'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'hotel'
 });
 
 function getSQL(callback) {
     connection.connect();
     let json = '';
     let query = 'SELECT * FROM clients';
-    connection.query(query, function(err, results, fields) {
+    connection.query(query, function (err, results, fields) {
         if (err)
             return callback(err, null);
 
         console.log('The query-result is: ', results[0]);
-
+        fs.writeFile('table.json', JSON.stringify(results), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
         json = JSON.stringify(results);
 
         connection.end();
@@ -26,11 +30,11 @@ function getSQL(callback) {
     });
 }
 
-http.createServer(function(req, res) {
-  getSQL(function(err, result) {
-    res.writeHead(200, {
-      'Content-Type' : 'x-application/json'
+http.createServer(function (req, res) {
+    getSQL(function (err, result) {
+        res.writeHead(200, {
+            'Content-Type': 'x-application/json'
+        });
+        res.end(result);
     });
-    res.end(result);
-  });
 }).listen(3000);
